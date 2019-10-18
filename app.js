197d5eea41f8b6d8588fad553d87799480f6ac1a
Mukul <mukul@Mukuls-MacBook-Air.local>
2019-10-18 10:30:56 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+// handlebar (hbs)=------------
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getDate', ()=>{
+	return new Date().getFullYear();
+	
+})
+
+hbs.registerHelper('scremIt', (text)=>{
+	return text.toUpperCase();
+});
+
+// handlebar middleware (app.use)
+
+app.use((req, res, next)=>{
+
+	var now = new Date().toString();
+	var log = now +' '+req.method +' '+req.url +'\n';
+	console.log(log);
+	fs.appendFile('server.log', log, (error)=>{
+		if(error){
+			console.log('Error in appending the file');
+		}
+	});
+	next();
+
+});
+
+
+
+
+// app.use((req, res, next)=>{
+// 	res.render('maintain.hbs');
+// });
+
+
+app.use(express.static(__dirname+'/public'));
+
+// hbs---------------------- 
+
+
+
+
+
+app.get('/', (req, res)=>{
+
+	res.render('home.hbs',{
+		tit:'Welcome to the Home Page',
+		mess:'Home Page',
+		desc:'You opened the home page'
+	});
+
+
+});
+
+
+
+
+
+app.get('/about', (req, res)=>{
+
+	res.render('about.hbs', {
+		tit:'Welcome to the About Page',
+		mess:'About Page',
+		desc:'You opened the about page'
+	});
+
+});
+
+app.listen(3000);
